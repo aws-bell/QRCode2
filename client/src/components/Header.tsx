706f14281ff581b65509122299139c5871b1b3cc
Doragon-RH <r216af@icloud.com>
2024-12-04 20:33:41 +0900

    headerの部分をコンポーネントとして再利用可能にした

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,20 @@
+export default function Header() {
+    return (
+        <div className="header">
+            <header className="bg-blue-200 py-4 px-8 flex justify-between items-center">
+                <h1 className="text-lg font-bold">QRコード生成アプリ</h1>
+                <nav className="flex space-x-4">
+                    <a href="favoriteQRList" className="text-sm text-gray-700 hover:underline">
+                        お気に入り
+                    </a>
+                    <a href="QRList" className="text-sm text-gray-700 hover:underline">
+                        履歴
+                    </a>
+                    <a href="/" className="text-sm text-gray-700 hover:underline">
+                        ログアウト
+                    </a>
+                </nav>
+            </header>
+        </div>
+    )
+}
